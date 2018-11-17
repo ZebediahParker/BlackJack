803c0bdfdcf9b9535cdc2d88ae9e9c777747f36d
ZebediahParker <zcp325@gmail.com>
2018-11-17 16:08:16 -0600

    Added functionality for initial cards being dealt and added structure for DealerHand and PlayerHand components

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import DealerHand from './DealerHand';
+import PlayerHand from './PlayerHand';
+import Deck from '../data/Deck';
 const background = 'images/background.jpg';
 
 const styles = {
@@ -10,11 +13,54 @@ const styles = {
 };
 
 class Game extends Component {
+    
+    state = {
+        deck: Deck,
+        cardBacks: "Simple Suits",
+        playerHand: [],
+        dealerHand: [],
+    };
+
+    getRandomCard = () => {
+
+        let card;
+
+        if(this.state.deck.length >= 0) {
+
+            let index = Math.floor(Math.random() * this.state.deck.length);
+            
+            card = this.state.deck[index];
+
+            this.state.deck.splice(index, 1);
+        }
+
+        return card;
+    }
+
+    dealCards = (cards, player = null, dealer = null) => {
+
+        for(let i = 0; i < cards; i++) {
+            if(player != null) {
+                player.push(this.getRandomCard());
+            }
+            if(dealer != null) {
+                dealer.push(this.getRandomCard());
+            }
+        }
+    };
+
+    startGame = () => {
+
+        this.dealCards(2, this.state.playerHand, this.state.dealerHand);
+
+    };
 
     render() {
         return (
             <div style={ styles.root }>
-
+                <button onClick={ () => this.startGame() } />
+                <DealerHand cardBacks dealerHand />
+                <PlayerHand cardBacks playerHand />
             </div>
         );
     }
